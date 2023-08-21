0859fa40ef6a667e448fab0e7d6b79fd98877a95
Sahil Batgeri <sahilbatgeri@gmail.com>
2023-08-22 00:58:30 +0530

    Added errors

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const ejs = require('ejs');
 const session = require('express-session');
 const path = require('path');
-const { log } = require('console');
+const { log, error } = require('console');
 const bcrypt = require('bcrypt');
 
 const app = express();
@@ -33,28 +33,129 @@ app.use(express.static(path.join('public')));
 
 
 app.get('/' , (req , res) => {
-    res.render('login');
+    res.render('login' , {errors : {} , email : '' , password : ''});
+})
+
+app.post('/' , (req , res) => {
+    const {email , password} = req.body;
+    const errors = {};
+    flag = true;
+
+    if(email == ''){
+        errors.emailisnull = true;
+        flag = false;
+    }
+
+    if(flag == true){
+        db.query('SELECT * FROM users WHERE email = ?' , [email] , async (err , results) => {
+            if(err){
+                console.log("email finding error" , err);
+            }
+            else{
+                if(results.length == 0){
+                    errors.invalidemail = true;
+                    flag = false;
+                    return;
+                }
+
+                if(password == ''){
+                    errors.passisnull = true;
+                    flag = false;
+                }
+
+                if(flag == true){
+                    const users = results[0];
+                    const match = await bcrypt.compare(password,users.password);
+                    if(!match){
+                        errors.invalidpassword = true;
+                        flag = false;
+                    }
+                }
+
+            }
+        });
+    }
+
+    if(Object.keys(errors).length > 0){
+        res.render('login' , {errors , email , password});
+    }
+
+    if(flag == true){
+        return res.redirect('/home');
+    }
+
 })
 
 app.get('/signup' , (req , res) =>{
-    res.render('signup');
+    res.render('signup' , {errors : {} , firstName : '' , lastName : '' , email : '' ,password : '' , confirmPassword: ''});
 })
 
 app.post('/signup' , async (req , res) => {
-    const {firstName , lastName , email , password} = req.body;
-
+    const {firstName , lastName , email , password , confirmPassword} = req.body;
+    const errors = {};
     const hashPassword = await bcrypt.hash(password , 10);
 
-    db.query('INSERT INTO users (username , email , password) VALUES (? ,?, ?)' , [firstName+" "+lastName , email , hashPassword] , async (err , results) => {
-        if(err){
-            console.log(' Data insertion error' , err);
-        }
-        else{
-            console.log('Data inserted');
-            res.redirect('/');
-        }
-    })
+    flag = true;
+
+    if(firstName == ''){
+        errors.fnameisnull = true;
+        flag = false;
+    }
+
+    if(lastName == ''){
+        errors.lnameisnull = true;
+        flag = false;
+    }
+
+    if(email == ''){
+        errors.emailisnull = true;
+        flag = false;
+    }
+
+    if(password == ''){
+        errors.passisnull = true;
+        flag = false;
+    }
+
+    if(password.length < 8 && flag == true){
+        errors.passwordLength = true;
+        flag = false;
+    }
+
+    if(confirmPassword == ''){
+        errors.confirmisnull = true;
+        flag =false;
+    }
+
+    if(confirmPassword != password && flag == true){
+        errors.notpasswordmatch = true;
+        flag = false;
+    }
+
+    if(Object.keys(errors).length > 0){
+       return res.render('signup' , {errors , email , firstName , password , confirmPassword , lastName});
+    }
+
+
+
+    if(flag == true){
+
+        db.query('INSERT INTO users (username , email , password) VALUES (? ,?, ?)' , [firstName+" "+lastName , email , hashPassword] , async (err , results) => {
+            if(err){
+                console.log(' Data insertion error' , err);
+            }
+            else{
     
+                console.log('Data inserted');
+                res.redirect('/');
+            }
+        });
+    }
+    
+})
+
+app.get('/home' , (req , res) => {
+    res.render('home');
 })
 
 app.listen(port, (req , res) => {
